d61e5a364a66283baa2f85a50dc53eff7602c896
patricknlnl <patricknlnlnl@gmail.com>
2024-12-30 19:18:33 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+Custom Component for Home Assistant: Lamp Energy Sensor
+This component calculates the total energy usage of lights based on brightness and wattage.
+"""
+
+# This file is intentionally empty as it initializes the custom integration.
